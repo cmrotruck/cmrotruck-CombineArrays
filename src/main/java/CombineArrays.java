6d2134b5286af155d051002cba8bc8d8259ad753
Chad Rotruck <crotruck@gmail.com>
2023-03-04 17:09:33 +0000

    Done

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,10 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int length = arr1.length + arr2.length;
+        int[] combinedArr = new int[length];
+        System.arraycopy(arr1, 0, combinedArr, 0, arr1.length);
+        System.arraycopy(arr2, 0, combinedArr, arr1.length, arr2.length);
+        return combinedArr;
     }
 }
